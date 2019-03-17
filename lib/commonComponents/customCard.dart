7db4b05a6b14b52b68fc7a46664193a2bad9fc92
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-03-17 21:15:53 +0530

    Display data from Firestore and insert new documents into it

--- lib/commonComponents/customCard.dart
+++ lib/commonComponents/customCard.dart
@@ -2,46 +2,50 @@ import 'package:flutter/material.dart';
 import '../secondPage.dart';
 
 class CustomCard extends StatelessWidget {
-  CustomCard({@required this.index});
+  CustomCard({@required this.title, this.description});
 
-  final index;
+  final title;
+  final description;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Column(
-      children: <Widget>[
-        Text('Card $index'),
-        FlatButton(
-            child: Text("Press Me"),
-            onPressed: () {
-              /** Push a named route to the stcak, which does not require data to be  passed */
-              // Navigator.pushNamed(context, "/a");
+        child: Container(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Column(
+              children: <Widget>[
+                Text(title),
+                FlatButton(
+                    child: Text("See More"),
+                    onPressed: () {
+                      /** Push a named route to the stcak, which does not require data to be  passed */
+                      // Navigator.pushNamed(context, "/a");
 
-              /** Create a new page and push it to stack each time the button is pressed */
-              // Navigator.push(context, MaterialPageRoute<void>(
-              //   builder: (BuildContext context) {
-              //     return Scaffold(
-              //       appBar: AppBar(title: Text('My Page')),
-              //       body: Center(
-              //         child: FlatButton(
-              //           child: Text('POP'),
-              //           onPressed: () {
-              //             Navigator.pop(context);
-              //           },
-              //         ),
-              //       ),
-              //     );
-              //   },
-              // ));
+                      /** Create a new page and push it to stack each time the button is pressed */
+                      // Navigator.push(context, MaterialPageRoute<void>(
+                      //   builder: (BuildContext context) {
+                      //     return Scaffold(
+                      //       appBar: AppBar(title: Text('My Page')),
+                      //       body: Center(
+                      //         child: FlatButton(
+                      //           child: Text('POP'),
+                      //           onPressed: () {
+                      //             Navigator.pop(context);
+                      //           },
+                      //         ),
+                      //       ),
+                      //     );
+                      //   },
+                      // ));
 
-              /** Push a new page while passing data to it */
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SecondPage(title: index)));
-            }),
-      ],
-    ));
+                      /** Push a new page while passing data to it */
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SecondPage(
+                                  title: title, description: description)));
+                    }),
+              ],
+            )));
   }
 }
--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  SecondPage({@required this.title});
+  SecondPage({@required this.title, this.description});
 
   final title;
+  final description;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Card No. $title'),
-      ),
-      body: Center(
-        child: RaisedButton(
-            child: Text('Back To HomeScreen'),
-            color: Theme.of(context).primaryColor,
-            textColor: Colors.white,
-            onPressed: () => Navigator.pop(context)),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(description),
+                RaisedButton(
+                    child: Text('Back To HomeScreen'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: () => Navigator.pop(context)),
+              ]),
+        ));
   }
 }
